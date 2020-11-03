062c2df49b719ff9b7ddb4633581a8b661e8117d
umutsunal <57930021+umutsunal@users.noreply.github.com>
2020-11-04 02:55:56 +0300

    Encrypt-decrypt add, ECB mode add
    Feistel encrypt-decrypt functions add, ECB encrypt-decrypt test success. Next update is CFC, OFC modes add.

--- bbm465/src/BBMcrypt.java
+++ bbm465/src/BBMcrypt.java
@@ -13,6 +13,7 @@ public class BBMcrypt {
 	static final int totalRounds = 10;
 	static final int blockSize = 96;
 	static final int keySize = 96;
+	static String initializationVector = "";
 	
 	
 	//SUBSTITION BOX. IT IS STORED AS A 2-DIMENSIONAL ARRAY
@@ -20,11 +21,11 @@ public class BBMcrypt {
 			{"0010","1100","0100","0001","0111","1010","1011","0110","1000","0101","0011","1111","1101","0000","1110","1001"},
 			{"1110","1011","0010","1100","0100","0111","1101","0001","0101","0000","1111","1010","0011","1001","1000","0110"},
 			{"0100","0010","0001","1011","1010","1101","0111","1000","1111","1001","1100","0101","0110","0011","0000","1110"},
-			{"1011","1000","1100","0111","0001","1110","0010","1011","0110","1111","0000","1001","1010","0100","0101","0011"},
-	};	//VALUES CHECKED 2 TIMES. NO TYPO.
+			{"1011","1000","1100","0111","0001","1110","0010","1101","0110","1111","0000","1001","1010","0100","0101","0011"},
+	};
 	
 	
-	//METHODS TO READ FILES
+	//METHODS TO READ FILES***********************************
 	public static String readKeyFile(String filename) {
 	    String keyFileString = "";
 		try {
@@ -73,8 +74,10 @@ public class BBMcrypt {
 		
 		return inputFileString;
 	}	//end of readKeyFile method	
+	//********************************************************
 	
-	//LEFT CIRCULAR SHIFT FOR SUBKEY GENERATION
+	
+	//LEFT CIRCULAR SHIFT FOR SUBKEY GENERATION***************
 	public static String leftCircularShift(String keyString) {
 		
 		String shiftedKey = "";
@@ -89,8 +92,10 @@ public class BBMcrypt {
 		shiftedKey = String.valueOf(charArray);
 		return shiftedKey;
 	}
-		
-	//SUBKEY GENERATION FUNCTION
+	//********************************************************
+	
+	
+	//SUBKEY GENERATION FUNCTION******************************
 	public static String[] subkeyGeneration(String keyString, int totalRounds) {
 		String[] finalKeyArray = new String[10];
 		String tempString = "";
@@ -102,23 +107,21 @@ public class BBMcrypt {
 			tempString = previousKey;	//96 bitlik
 			tempString = leftCircularShift(tempString);	//96 bit
 			previousKey = tempString;	//96 bit, shiftlenmiş
-			System.out.println(tempString);
+			//System.out.println(tempString);
 			//permuted choice
 			char[] tempCharArray = tempString.toCharArray();
 			
 			if(i%2==0) {		
-				System.out.println("%2==0");
-				
 				for(int j=0; j<blockSize;j=j+2) {
 					finalTempString = finalTempString.concat(String.valueOf(tempCharArray[j]));
 				}
 			}
 			else {
-				System.out.println("%2==1");
 				for(int j=1; j<blockSize;j=j+2) {
 					finalTempString = finalTempString.concat(String.valueOf(tempCharArray[j]));
 				}				
 			}
+			System.out.println(i + "subkey: " + finalTempString);
 		finalKeyArray[i] = finalTempString;
 		finalTempString = "";
 		}
@@ -126,8 +129,9 @@ public class BBMcrypt {
 		
 		return finalKeyArray;
 	}
+	//********************************************************
 	
-	//XOR FUNCTION
+	//XOR FUNCTION********************************************
     static String xorFunction(String firstString, String secondString, int stringSize){ 
     String resultString = ""; 
           
@@ -143,25 +147,28 @@ public class BBMcrypt {
         } 
         return resultString; 
     } 
-	
-    //SUBSTITUTION BOX FUNCTION
+	//********************************************************
+    
+    //SUBSTITUTION BOX FUNCTION*******************************
     public static String[] substitutionBoxFunction(String[] xORedArray, String[][] substitutionBox) {
     	String[] resultArray = new String[12];
     	String tempString, outerBits, innerBits;
     	tempString = outerBits = innerBits = "";
     	
     	for(int i=0;i<12;i++) {
-    		tempString = xORedArray[i];
+    		tempString = xORedArray[i];					
     		
     		outerBits = outerBits.concat(String.valueOf(tempString.charAt(0))).concat(String.valueOf(tempString.charAt(5)));
     		innerBits = innerBits.concat(String.valueOf(tempString.charAt(1))).concat(String.valueOf(tempString.charAt(2))).concat(String.valueOf(tempString.charAt(3))).concat(String.valueOf(tempString.charAt(4)));
     		
     		resultArray[i] = substitutionBox[Integer.parseInt(outerBits, 2)][Integer.parseInt(innerBits, 2)];
+    		outerBits = innerBits = "";
     	}
     	return resultArray;
     }
+    //********************************************************
     
-    //PERMUTATION FUNCTION FOR SCRAMBLE FUNCTION
+    //PERMUTATION FUNCTION FOR SCRAMBLE FUNCTION**************
     public static String permutationFunction(String substitutedString) {
     	char tempChar;
     	
@@ -176,9 +183,9 @@ public class BBMcrypt {
     	String resultString = String.valueOf(charArray);
     	return resultString;
     }
+    //********************************************************
     
-    
-	//SCRAMBLE FUNCTION (FINISHED BUT NOT TESTED)
+	//SCRAMBLE FUNCTION (FINISHED BUT NOT TESTED)*************
 	public static String scrambleFunction(String rightSubText, String subKey, String[][] substitution) {
 		
 		String xorResult = xorFunction(rightSubText, subKey, blockSize/2);
@@ -190,7 +197,7 @@ public class BBMcrypt {
 		}
 		
 		for(int j = 0; j<4; j++) {
-			xORedArray[j+8] = xorFunction(xORedArray[j], xORedArray[j*2], xORedArray[j*2+1].length());
+			xORedArray[j+8] = xorFunction(xORedArray[j*2], xORedArray[j*2+1], xORedArray[j*2+1].length());
 		}
 		
 		//NOW USE SUBSTITUTION BOX ON XORED ARRAY(12 ELEMENTS OF 6 BITS)
@@ -203,36 +210,70 @@ public class BBMcrypt {
 		String resultString = permutationFunction(substitutedString);	
 		return resultString;
 	}
-	
+	//********************************************************
 	
 	//METHOD FOR FEISTEL CIPHER
-	public static String feistelCipherEncryption(String originalText, String keyString) {
+	public static String feistelCipherEncryption(String originalText, String[] subKeyArray) {
 		
-		String encryptedBlock = "";
+		String encryptedBlock, leftZero, rightZero;
+		encryptedBlock = leftZero = rightZero = "";
 		
+		leftZero = originalText.substring(0, blockSize/2);
+		rightZero = originalText.substring(blockSize/2, blockSize);
 		
+		String[] leftBlocksArray = new String[11];
+		String[] rightBlocksArray = new String[11];
+		
+		leftBlocksArray[0] = leftZero;
+		rightBlocksArray[0] = rightZero;
+		
+		for (int i=1; i<11; i++) {
+			leftBlocksArray[i] = rightBlocksArray[i-1];
+			rightBlocksArray[i] = xorFunction(leftBlocksArray[i-1], scrambleFunction(rightBlocksArray[i-1], subKeyArray[i-1], substitution), 48); 	
+			System.out.println(i-1 + "scramble function " + scrambleFunction(rightBlocksArray[i-1], subKeyArray[i-1], substitution));
+		
+		}
+		
+		encryptedBlock = encryptedBlock.concat(leftBlocksArray[10]).concat(rightBlocksArray[10]);
 		
 		
 		return encryptedBlock;
 	}
 	
-	public static String feistelCipherDecryption(String originalText, String keyString) {
+	public static String feistelCipherDecryption(String originalText, String[] subKeyArray) {
 		
-		String finalText = "";
+		String decryptedBlock, leftTen, rightTen;
+		decryptedBlock = leftTen = rightTen = "";
 		
+		leftTen = originalText.substring(0, blockSize/2);
+		rightTen = originalText.substring(blockSize/2, blockSize);
 		
-		return finalText;
+		String[] leftBlocksArray = new String[11];
+		String[] rightBlocksArray = new String[11];		
+		
+		leftBlocksArray[10] = leftTen;
+		rightBlocksArray[10] = rightTen;
+		
+		for (int i=9; i>-1; i--) {
+			rightBlocksArray[i] = leftBlocksArray[i+1];
+			
+			leftBlocksArray[i] = xorFunction(rightBlocksArray[i+1], scrambleFunction(leftBlocksArray[i+1], subKeyArray[i], substitution), 48); 	
+			System.out.println(i + "scramble function " + scrambleFunction(leftBlocksArray[i+1], subKeyArray[i], substitution));
+		
+		}
+		
+		decryptedBlock = decryptedBlock.concat(leftBlocksArray[0]).concat(rightBlocksArray[0]);
+		
+		return decryptedBlock;
 	}	
-	
-	
-	
+
 	//METHODS TO ENCRYPT-DECRYPT TEXTS
-	public static String ECBEncryption(String originalText, String keyString) {
+	public static String ECBEncryption(String originalText, String[] subKeyArray) {
 		
 		String finalText = "";
 		
 		if(originalText.length()==96) {	//if string length<=96, there are only 1 block
-			finalText = feistelCipherEncryption(originalText, keyString);
+			finalText = feistelCipherEncryption(originalText, subKeyArray);
 		}
 		else {	//if string length>96, there are more than 1 block 
 			int numberOfBlocks = originalText.length()/96;
@@ -241,7 +282,7 @@ public class BBMcrypt {
 			String tempStringBlock = "";
 			
 			for(int i=0;i<numberOfBlocks;i++) {
-				tempStringBlock = feistelCipherEncryption(originalText.substring(i*blockSize, (i+1)*blockSize), keyString);
+				tempStringBlock = feistelCipherEncryption(originalText.substring(i*blockSize, (i+1)*blockSize), subKeyArray);
 				System.out.println(tempStringBlock.length());
 				finalText = finalText.concat(tempStringBlock);				
 			}
@@ -249,6 +290,67 @@ public class BBMcrypt {
 		return finalText;
 	}
 	
+	public static String ECBDecryption(String cipherText, String[] subKeyArray) {
+		
+		String finalText = "";
+		
+		if(cipherText.length()==96) {	//if string length<=96, there are only 1 block
+			finalText = feistelCipherDecryption(cipherText, subKeyArray);
+		}
+		else {	//if string length>96, there are more than 1 block 
+			int numberOfBlocks = cipherText.length()/96;
+			System.out.println("numofblocks: " + numberOfBlocks);
+		
+			String tempStringBlock = "";
+			
+			for(int i=0;i<numberOfBlocks;i++) {
+				tempStringBlock = feistelCipherDecryption(cipherText.substring(i*blockSize, (i+1)*blockSize), subKeyArray);
+				System.out.println(tempStringBlock.length());
+				finalText = finalText.concat(tempStringBlock);				
+			}
+		}
+		return finalText;
+	}
+	
+	public static String CBCEncryption(String originalText, String[] subKeyArray) {
+		String finalText = "";
+		
+		if(originalText.length()==96) {	//if string length<=96, there are only 1 block
+			finalText = feistelCipherEncryption(originalText, subKeyArray);
+		}
+		else {	//if string length>96, there are more than 1 block 
+			int numberOfBlocks = originalText.length()/96;
+			System.out.println("numofblocks: " + numberOfBlocks);
+		
+			String tempStringBlock = "";
+			
+
+		}
+		
+		
+		return finalText;
+	}
+	
+	public static String CBCDecryption(String cipherText, String[] subKeyArray) {
+		String finalText = "";
+		
+		
+		return finalText;
+	}
+	
+	public static String OFCEncryption(String originalText, String[] subKeyArray) {
+		String finalText = "";
+		
+		
+		return finalText;
+	}
+	
+	public static String OFCDecryption(String cipherText, String[] subKeyArray) {
+		String finalText = "";
+		
+		
+		return finalText;
+	}	
 	
 	
 	public static void main(String[] args) {
@@ -327,23 +429,38 @@ public class BBMcrypt {
 	    //**************************************************************************//
 	    
 	    
-	    String[] subkeyArray = subkeyGeneration(decodedKeyString, totalRounds);
+	    String[] subKeyArray = subkeyGeneration(decodedKeyString, totalRounds);
 
 	    
 	    //**************************************************************************//
-		//SUBKEYS ARE GENERATED. NOW STARTING TO IMPLEMENT THE FEISTEL CIPHER		//
+		//SUBKEYS ARE GENERATED. NOW INITIALIZING THE INITIALIZATION VECTOR 		//
 	    //**************************************************************************//	    
+		
+	    for(int i=0;i<96;i++) {
+			initializationVector = initializationVector.concat("1");
+		}
 	    
+	    //**************************************************************************//
+		//VECTOR IS INITIALIZED. NOW STARTING TO IMPLEMENT THE FEISTEL CIPHER		//
+	    //**************************************************************************//	    	    
 	    
 	    String finalString = "";
 	    
-	    /*if(actionType.equals("enc") && modeType.equals("ECB")) {
+	    if(actionType.equals("enc") && modeType.equals("ECB")) {
 	    	System.out.println("*********");	
-	    	finalString = ECBEncryption(inputText, decodedKeyString);
-	    }*/
+	    	finalString = ECBEncryption(inputText, subKeyArray);
+	    	System.out.println("FINAL STRING FOR ECB ENCRYPTION:");
+	    	System.out.println(finalString);
+	    }
+	    else if(actionType.equals("dec") && modeType.equals("ECB")) {
+	    	System.out.println("*********");	
+	    	finalString = ECBDecryption(inputText, subKeyArray);
+	    	System.out.println("FINAL STRING FOR ECB DECRYPTION:");
+	    	System.out.println(finalString);
+	    }
 	    
 	    
-	    scrambleFunction("100101111001010000100101011001101011111101000010","011001001100010010110001011100100010111000000000",substitution);		 
+	    //scrambleFunction("100101111001010000100101011001101011111101000010","011001001100010010110001011100100010111000000000",substitution);		 
 	    
 		
 		
